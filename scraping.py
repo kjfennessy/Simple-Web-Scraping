fda7e69c204c62cb2a434607daa86ce8adad3f2b
kjfennessy <kellyjfennessy@gmail.com>
2016-09-03 17:35:08 -0400

    first commit

---
+++ scraping.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+import csv
+
+locationsList = []
+
+for number in range(1,17):
+	url = "http://rovingpanda.com/4698/16-places-to-explore-in-your-20s/{}".format(number)
+	r = requests.get(url).content
+	soup = bs4.BeautifulSoup(r, "html.parser")
+	#inspect elements, find where desired element is in html
+	locations = soup.findAll('h1')
+	locationsList.append(locations[1].getText())
+
+print locationsList
+
